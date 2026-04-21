e7c6701877cba5f718046401012e2c85ecd7808b
seth superbad <rossford186@gmail.com>
2026-04-21 11:39:44 -0500

    Update chatfunction.cpp
    forgot to add function rva

--- chatfunction.cpp
+++ chatfunction.cpp
@@ -2,6 +2,8 @@ using Chat = void( __fastcall* )( __int64 a1, unsigned int a2 );
 
 inline Chat chat = nullptr;
 
+chat = reinterpret_cast< Chat >( Memory::image_base + 0x65EA810 );
+
 void send_chat( __int64 message, int chattype )
 {
     chat
